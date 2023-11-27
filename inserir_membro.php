2d0b17afa928d99f91bd3cb44221e4249864c4e2
kayfidelis <kayquerodrigues.f@gmail.com>
2023-11-26 22:31:09 -0300

    algumas alterações referente ao método de inserir membros

--- inserir_membro.php
+++ inserir_membro.php
@@ -5,6 +5,7 @@ include 'conexao.php';
 $nome = $_POST['txtnome'];
 $email = $_POST['txtemail'];
 $senha = $_POST['txtsenha'];
+$data = $_POST['data'];
 $telefone = $_POST['txttel'];
 $unidade = $_POST['txtunidade'];
 $criptografada = md5($senha);
@@ -14,17 +15,21 @@ $exibe = $consulta->fetch(PDO::FETCH_ASSOC);
 
 
 if ($consulta->rowCount() == 1) {
-    header('location:erro1.php');
+    echo '<script>
+    alert("O email digitado já está cadastro!");
+    window.location.href = "FormLogin.php";
+  </script>';
 
 }else{
 
-        $incluir = $cn->prepare("call inserir_membros(?, ?, ?, 0, ?, ?)");
+        $incluir = $cn->prepare("call inserir_membros(?, ?, ?, 0, ?, ?, ?)");
     
         $incluir->bindParam(1, $unidade);
         $incluir->bindParam(2, $nome);
         $incluir->bindParam(3, $criptografada);
-        $incluir->bindParam(4, $telefone);
-        $incluir->bindParam(5, $email);
+        $incluir->bindParam(4, $data);
+        $incluir->bindParam(5, $telefone);
+        $incluir->bindParam(6, $email);
 
         $incluir->execute();
 
